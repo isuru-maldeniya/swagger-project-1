89174d2d33f29b2c7a7c54fd091967bf60156893
DELL <isurumaldeiya@gmail.com>
2021-09-25 21:14:14 +0530

    swwagger ui correctly displayed

--- src/main/java/com/exventure/training/swaggerproject1/Controller/CustomerController.java
+++ src/main/java/com/exventure/training/swaggerproject1/Controller/CustomerController.java
@@ -1,11 +1,40 @@
 package com.exventure.training.swaggerproject1.Controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.exventure.training.swaggerproject1.Controller.DTO.CustomerDTO;
+import com.exventure.training.swaggerproject1.Services.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/customer")
 public class CustomerController {
+    @Autowired
+    private CustomerService customerService;
 
+    @PostMapping(value = "/addcustomer")
+    public void addCustomer(@RequestBody CustomerDTO dto){
+        customerService.addCustomer(dto);
+    }
 
+    @PutMapping(value = "/updatecustmer")
+    public void updatedCustomer(@RequestBody CustomerDTO dto){
+        customerService.updateCustomer(dto);
+    }
+
+    @GetMapping(value = "/getallcustomee")
+    public List<CustomerDTO> getALllCustomers(){
+        return customerService.getAllCustomers();
+    }
+
+    @GetMapping(value = "/getcustomer/{id}")
+    public CustomerDTO getCustomer(@PathVariable int id){
+        return customerService.getCustomer(id);
+    }
+
+    @DeleteMapping(value = "/delete/{id}")
+    public void deleteCustomer(@PathVariable int id){
+        customerService.deleteCustomer(id);
+    }
 }
